ab42b6462f7061461ad247a8445722399f57d2c2
Edinei da Motta Martinello <edinei128@hotmail.com>
2026-03-01 18:01:49 -0300

    secao06

--- famp/secao06/criar_tabelas.py
+++ famp/secao06/criar_tabelas.py
@@ -1,13 +1,14 @@
 from core.configs import settings
 from core.database import engine
+from core.configs import DBBaseModel
 
 
 async def criar_tabelas() -> None:
     import models.__all_models
     print('Criando tabelas no banco de dados...')
     async with engine.begin() as conn:
-        await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
-        await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+        await conn.run_sync(DBBaseModel.metadata.drop_all)
+        await conn.run_sync(DBBaseModel.metadata.create_all)
     print('Tabelas criadas com sucesso!')
 
 if __name__ == '__main__':
